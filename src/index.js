f0750f104f1288c23cc98a784a7676974b2fd73e
Roman Przybylski <romprzy81@gmail.com>
2024-09-03 16:08:30 +0200

    init: Git UI tutorial from https://blog.jamalhaija.com/build-your-own-component-library-plugin-with-vue-3-and-rollup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import MyButton from './components/MyButton.vue'
+
+const components = {
+  MyButton
+}
+
+export default app => {
+  for (const prop in components) {
+    if (components.hasOwnProperty(prop)) {
+      app.component(
+        components[prop].name,
+        components[prop]
+      )
+    }
+  }
+}
